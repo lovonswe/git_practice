b809cce1376455c7cede2076fd865f69470130f6
nahidswe <nahidswe@gmail.com>
2023-02-02 23:20:49 +0600

    multiplication.cpp added

---
+++ multiplication.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ll t;
+    while(t--){
+        ll a, b, mul;
+        cin >> a >> b;
+        mul = a*b;
+        cout << mul << endl;
+    }
+}
