54f44d98fc0fd55ee43714df21d171613cd06608
nahidswe <nahidswe@gmail.com>
2023-02-02 18:38:31 +0600

    add two number a and b

--- sum.cpp
+++ sum.cpp
@@ -5,5 +5,12 @@ using namespace std;
 #define vctr vector<ll>
 
 int main(){
-
+    ll t;
+    cin >> t;
+    while(t--){
+        ll a, b;
+        ll sum;
+        sum=a+b;
+        cout << sum << endl;
+    }
 }
